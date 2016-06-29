b5d4ac7e69f23083ae6d9acc671e10a7231c15c9
simoneslatnpou <svbyogibear@gmail.com>
2016-06-29 22:37:33 +0200

    Initial commit.

---
+++ 20160628_SMSPortal_Challenge-4_Q3/Prefix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _20160628_SMSPortal_Challenge_4_Q3 {
+    class Prefix {
+        public static IEnumerable<string> Prefixes(IEnumerable<string> words, int length) {
+            return (from word in words.Distinct()
+                    where words.Any(other =>
+                        word.Length >= length && word.StartsWith(other.Substring(0, length)))
+                    select word.Substring(0, length)).Distinct();
+        }
+
+        static void Main(string[] args) {
+            foreach (var p in Prefixes(new string[] { "many", "manly", "men", "maybe", "my" }, 3))
+                Console.WriteLine(p);
+            Console.ReadLine();
+        }
+    }
+}
